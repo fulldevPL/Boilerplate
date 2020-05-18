70fc0460bf5923743395582af92ed0aff1c0e3b5
Maciej Szeremeta <maciekj5@o2.pl>
2020-05-18 23:48:34 +0200

    add webpack simple function

---
+++ config/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    script: './src/index.js',
+  },
+  output: {
+    filename: '[name].[contenthash:6].js',
+    path: path.resolve(__dirname, '../', 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 8080,
+    contentBase: path.resolve(__dirname, '../', 'public'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'My app',
+      template: 'src/index.html',
+    }),
+  ],
+};

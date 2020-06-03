90cffcd42dcb0edcdb8d67b88f54c1c2a53d7fdb
Maciej <fulldevPL@gmail.com>
2020-06-03 12:38:39 +0200

    mod direction2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // Import js file
 import {message, messageDOM} from './tools/message';
+import './components/footer';
 
 //Import txt file
 import info from './data/title.txt';
 
 // Import scss file
-import './scss/style.scss';
+import './sass/index.scss';
 
 message('test import function');
 messageDOM(info);
